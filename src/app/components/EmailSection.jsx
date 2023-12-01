e138aabbbb201a4a682ac88843e59a9a52658773
werhani wassim <werhaniwassim@kelibia.r-iset.tn>
2023-12-01 14:21:30 +0100

    change

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -39,7 +39,7 @@ const EmailSection = () => {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">let's connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">lets connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
         <div className="menu hidden  md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
-              <li>
+              <li key={index}>
                 <NavLink href={link.path} title={link.title} />
               </li>
             ))}
